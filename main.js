2c9512c6f6689a2653107d3e0e103d94526f16ec
JBattishill <jbattishill91@gmail.com>
2023-10-02 19:58:14 +1100

    anothertest

--- main.js
+++ main.js
@@ -8,12 +8,14 @@ clientSecret = "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"
 
 var searchText = keyword1 + keyword2 + keyword3 + "music";
 
+
 function authenticate() {
     return gapi.auth2.getAuthInstance()
         .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
         .then(function() { console.log("Sign-in successful"); },
               function(err) { console.error("Error signing in", err); });
   }
+
   function loadClient() {
     gapi.client.setApiKey(apiKey);
     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
